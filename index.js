6f31bad610232c66b986c978f1e255224e7bd52b
Parsa <parsa.firoozi55@gmail.com>
2021-11-29 16:45:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Insta = require('@androz2091/insta.js');
 const Discord = require('discord.js');
 
 const { 
-
     webhook_id, 
     webhook_token 
 } = require('./config.json')
@@ -11,17 +10,17 @@ const client = new Insta.Client();
 const webhookClient = new Discord.WebhookClient(webhook_id, webhook_token);
 
 
-client.on('connected', () => {
+client.on('connected', () => 
+{
     console.log(`Logged in as ${client.user.username}`);
 });
  
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', (message) =>
+{
   if (message.author.id === client.user.id) return
-
-
+    
   const embed1 = new Discord.MessageEmbed()
-
   .setDescription(`<:instagram:798775886668103680> **یک نفر برای شما پیامی جدید را در اینستاگرام ارسال کرد** <:instagram:798775886668103680>`)
   .addField("> 📧 نام فرستنده", `> ${message.author.username}`)
   .addField("> 🆔 ایدی فرستنده پیام", `> ${message.author.id}`)
@@ -29,12 +28,12 @@ client.on('messageCreate', (message) => {
   .setColor('#833AB4')
   .setTimestamp();
 
-  webhookClient.send({
+  webhookClient.send(
+  {
       embeds: [embed1]
   });
 
 
-    });
+});
  
-
 client.login('Username_IG', 'Password_IG'); 
